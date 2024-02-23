877145f01c7120737c67e3410c81b51e7ad5c3e6
shihch3 <chien-cheng.shih@novartis.com>
2024-02-23 12:18:46 -0800

    test git

--- cellpose_singlecell.py
+++ cellpose_singlecell.py
@@ -11,10 +11,13 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.colors import LinearSegmentedColormap
 
+# test
 # import cellpoose
 # combine files
 prjdir = Path("/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/shihch3/projects/HPA_single_data")
-datadir = Path("/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/BenchmarkDatasets/hpa-single-cell-image-classification")
+datadir = Path(
+    "/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/BenchmarkDatasets/hpa-single-cell-image-classification"
+)
 HPAdir = Path("/dlab/ldrive/CBT/USLJ-DSDE_DATA-I10008/shihch3/projects/HPA_single_data")
 
 # file_csv = datadir.joinpath("train_select_1K.csv")
@@ -24,55 +27,59 @@ file_csv = datadir.joinpath("train.csv")
 df = pd.read_csv(file_csv)
 print(df)
 
+
 # %%
-def load_img(ID, channels = [0, 1, 2, 3], total_ch = 4):
-    channel_name_dict = { 
-        0: 'blue', # nucleus
-        1: 'green', # protein
-        2: 'red', # Microtubule
-        3: 'yellow' # ER
+def load_img(ID, channels=[0, 1, 2, 3], total_ch=4):
+    channel_name_dict = {
+        0: "blue",  # nucleus
+        1: "green",  # protein
+        2: "red",  # Microtubule
+        3: "yellow",  # ER
     }
-    if channels == 'all': 
+    if channels == "all":
         channels = list(range(total_ch))
     img = []
     for channel in channels:
         channel_name = channel_name_dict[channel]
-        file_path = datadir.joinpath('train', ID + '_' + channel_name + '.png')
+        file_path = datadir.joinpath("train", ID + "_" + channel_name + ".png")
         img.append(imread(file_path))
-    img = np.stack(img, axis = -1)
+    img = np.stack(img, axis=-1)
     return img
 
+
 def img_display(img):
-    channel_name_dict = { 
-        0: LinearSegmentedColormap.from_list("B", ["#000", "#00f"]), # nucleus
-        1: LinearSegmentedColormap.from_list("G", ["#000", "#0f0"]), # protein
-        2: LinearSegmentedColormap.from_list("R", ["#000", "#f00"]), # Microtubule
-        3: LinearSegmentedColormap.from_list("Y", ["#000", "#ff0"]) # ER
+    channel_name_dict = {
+        0: LinearSegmentedColormap.from_list("B", ["#000", "#00f"]),  # nucleus
+        1: LinearSegmentedColormap.from_list("G", ["#000", "#0f0"]),  # protein
+        2: LinearSegmentedColormap.from_list("R", ["#000", "#f00"]),  # Microtubule
+        3: LinearSegmentedColormap.from_list("Y", ["#000", "#ff0"]),  # ER
     }
 
-    fig, axarr = plt.subplots(1, img.shape[2], figsize = (20, 5))
+    fig, axarr = plt.subplots(1, img.shape[2], figsize=(20, 5))
     for i in range(img.shape[2]):
         cmap = channel_name_dict[i]
         axarr[i].imshow(img[:, :, i], cmap=cmap)
-        axarr[i].axis('off')
+        axarr[i].axis("off")
     plt.show()
+
+
 # %%
 new_row = []
 for i, row in tqdm(df[:10].iterrows()):
-    file_id = row['ID']
-    file_path = datadir.joinpath('train', file_id)
-    
-    channels_order = [2,1,0]
+    file_id = row["ID"]
+    file_path = datadir.joinpath("train", file_id)
+
+    channels_order = [2, 1, 0]
     img = load_img(file_id, channels=channels_order)
-    
-    oppath = HPAdir.joinpath('train_rgb_c210', file_id + '.png')
+
+    oppath = HPAdir.joinpath("train_rgb_c210", file_id + ".png")
     oppath.parent.mkdir(parents=True, exist_ok=True)
     imsave(oppath, img)
-    row['file_path'] = str(oppath)
-    row['channel_order'] = ', '.join([str(x) for x in channels_order]) 
-    row['channel_counts'] = len(channels_order)
-    row['frame_size_y'] = img.shape[0]
-    row['frame_size_x'] = img.shape[1]
+    row["file_path"] = str(oppath)
+    row["channel_order"] = ", ".join([str(x) for x in channels_order])
+    row["channel_counts"] = len(channels_order)
+    row["frame_size_y"] = img.shape[0]
+    row["frame_size_x"] = img.shape[1]
     new_row.append(row)
     # img_display(img)
 
@@ -83,26 +90,30 @@ for i, row in tqdm(df[:10].iterrows()):
     # outlines = utils.outlines_list(masks)
     # plt.imshow(img)
     # for o in outlines:
-    #     plt.plot(o[:, 0], o[:, 1], 'r') 
-    
+    #     plt.plot(o[:, 0], o[:, 1], 'r')
 
-new_row = pd.concat(new_row,ignore_index=True)
-opcsvpath = datadir.joinpath('train_rgb_c210.csv')
+
+new_row = pd.concat(new_row, ignore_index=True)
+opcsvpath = datadir.joinpath("train_rgb_c210.csv")
 new_row.to_csv(opcsvpath, index=False)
 # %%
 
-file_id = '1cb6bd56-bba5-11e8-b2ba-ac1f6b6435d0'
+file_id = "1cb6bd56-bba5-11e8-b2ba-ac1f6b6435d0"
 img = load_img(file_id, channels=[0, 1, 2, 3])
 img_display(img)
 # %%
-img_merge = imread(HPAdir.joinpath('train_rgb_c210_merge', 'img', file_id + '.png'))
-img_merge = np.stack([img_merge[:, :, 2], img_merge[:, :, 1], img_merge[:, :, 0]], axis = -1)
+img_merge = imread(HPAdir.joinpath("train_rgb_c210_merge", "img", file_id + ".png"))
+img_merge = np.stack(
+    [img_merge[:, :, 2], img_merge[:, :, 1], img_merge[:, :, 0]], axis=-1
+)
 img_display(img_merge)
 # %%
 
-img_seg = imread(HPAdir.joinpath('train_rgb_c210_merge', 'seg', file_id + '_cp_output.png'))
+img_seg = imread(
+    HPAdir.joinpath("train_rgb_c210_merge", "seg", file_id + "_cp_output.png")
+)
 plt.imshow(img_seg)
-plt.axis('off')
+plt.axis("off")
 
 
 # %%
